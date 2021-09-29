6439431f30053709d88815d653a36b2037cd9a6b
Liam McFadden <liam.e.mcfadden@gmail.com>
2021-09-29 11:42:33 -0400

    Whoops, actually add files

--- src/App.js
+++ src/App.js
@@ -1,65 +1,80 @@
-import { useState } from 'react'
 import { animated, useChain, useSpring, useSpringRef } from 'react-spring'
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleRotated } from './redux/rotatedSlice';
+import { setHoveredMenuItem } from './redux/hoveredMenuItemSlice';
+import { setAnimating } from './redux/animatingSlice';
+import backIcon from './arrow_back_ios_white_24dp.svg'
 import logoBack from './logo_and_back.svg';
 import redBackground from './red_back.svg';
 import './App.css';
 
 function App() {
 
+  const dispatch = useDispatch()
+
+  const rotated = useSelector((state) => state.rotated.value)
+  const animating = useSelector((state) => state.animating.value)
+  const hoveredMenuItem = useSelector((state) => state.hoveredMenuItem.value)
+
   const underlineStyleByMenuItem = {
-    'project': {marginLeft: '13vw', width: '9vw'},
+    'projects': {marginLeft: '13vw', width: '9vw'},
     'about': {marginLeft: '2vw', width: '6vw'},
     'contact': {marginLeft: '26.4vw', width: '8.5vw'},
   }
-
-  const [state, toggle] = useState(false)
-  const [hoveredMenuItem, hoverMenuItem] = useState('project')
 
   const disappearRef = useSpringRef()
   const rotateRef = useSpringRef()
   const slideRef = useSpringRef()
 
   const rotateProps = useSpring({
-    to: {rotateZ: state ? -45 : 0.00001},
-    ref: rotateRef
+    to: {rotateZ: rotated ? -45 : 0.00001},
+    ref: rotateRef,
+    onStart: () => dispatch(setAnimating(true)),
+    onRest: () => dispatch(setAnimating(false))
   })
 
   const slideProps = useSpring({
     to: {
-      translateX: state ? '40vw' : '0vw',
-      translateY: state ? '40vw' : '0vw'
+      translateX: rotated ? '40vw' : '0vw',
+      translateY: rotated ? '40vw' : '0vw'
     },
-    ref: slideRef
+    ref: slideRef,
+    onStart: () => dispatch(setAnimating(true)),
+    onRest: () => dispatch(setAnimating(false))
   })
 
   const disappearProps = useSpring({
     to: {
-      translateY: state ? '10vw' : '0vw',
-      translateX: state ? '-10vw' : '0vw',
+      translateY: rotated ? '10vw' : '0vw',
+      translateX: rotated ? '-10vw' : '0vw',
       rotateZ: '45deg'
     },
-    ref: disappearRef
+    ref: disappearRef,
+    onStart: () => dispatch(setAnimating(true)),
+    onRest: () => dispatch(setAnimating(false))
   })
 
   const underlineProps = useSpring({
     to: underlineStyleByMenuItem[hoveredMenuItem]
   })
 
-  useChain([disappearRef, rotateRef, slideRef], [0, 0.2, 0.5])
+  useChain(rotated ? [disappearRef, rotateRef, slideRef] : [slideRef, rotateRef, disappearRef], [0, 0.2, 0.5])
 
   return (
     <div className="App">
       <header className="App-header">
-        <animated.div style={disappearProps} className={state ? "Menu-z0" : "Menu-z5"} >
-          <div className="Menu-item" onClick={() => toggle(!state)} onMouseEnter={() => hoverMenuItem('about')}> ABOUT </div>
-          <div className="Menu-item" onClick={() => toggle(!state)} onMouseEnter={() => hoverMenuItem('project')}> PROJECTS </div>
-          <div className="Menu-item" onClick={() => toggle(!state)} onMouseEnter={() => hoverMenuItem('contact')}> CONTACT </div>
+        <animated.div style={disappearProps} className={(rotated || animating) ? "Menu-z0" : "Menu-z5"} >
+          <div className="Menu-item" onClick={() => dispatch(toggleRotated())} onMouseEnter={() => dispatch(setHoveredMenuItem('about'))}> ABOUT </div>
+          <div className="Menu-item" onClick={() => dispatch(toggleRotated())} onMouseEnter={() => dispatch(setHoveredMenuItem('projects'))}> PROJECTS </div>
+          <div className="Menu-item" onClick={() => dispatch(toggleRotated())} onMouseEnter={() => dispatch(setHoveredMenuItem('contact'))}> CONTACT </div>
           <animated.div style={underlineProps} className="underline"></animated.div>
         </animated.div>
         <animated.div style={rotateProps} className="background-graphics">
-          <img src={redBackground} className="red-back" alt="logo" />
+          <img src={backIcon} onClick={() => dispatch(toggleRotated())} className="back-button" alt="" />
+          <div className="Page-title"> {hoveredMenuItem.toUpperCase()} </div>
+          <img src={redBackground} className="red-back" alt="" />
           <animated.div style={slideProps}> 
-            <img src={logoBack} className="logo-back" alt="logo" />
+            <img src={logoBack} className="logo-back" alt="" />
           </animated.div>
         </animated.div>
       </header>
